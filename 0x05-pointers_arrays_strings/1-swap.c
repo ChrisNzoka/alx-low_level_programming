0bf96741546753dbf4941da94776efc4c997f936
Chris Nzoka-okoye <chrisnzokaokoye@gmail.com>
2022-03-21 21:30:19 +0100

    swapped address

---
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -0,0 +1,17 @@
+/**
+ * swap_int - swaps the value of the variable
+ *
+ * @a: pointer to the variable a
+ * @b: pointer to the variable b
+ *
+ * @date 2022-03-21
+ *
+ * @copyright Copyright (c) 2022
+ *
+ */
+
+void swap_int(int *a, int *b)
+{
+*a = 42;
+*b = 98;
+}
